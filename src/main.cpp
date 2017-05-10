9f52cb397b6ae6145c675cd0e243a07a739a7278
Kayne Ruse <kayneruse@gmail.com>
2017-05-10 19:21:21 +1000

    got carried away with unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,58 @@
 #include "symbol.hpp"
 
+#include <functional>
 #include <iostream>
 
-std::string toString(Symbol);
-//Symbol parseString(std::string);
+//unit test utilities
+static int unitCount = 0;
+static int failCount = 0;
+void unit(std::string name, std::function<bool()> f) {
+  std::cout << name << "\t\t" <<
+  (f() ? (unitCount++, "OK") : (failCount++, "fail"))
+  << std::endl;
+}
 
+//forward declarations
+std::string toString(Symbol symbol);
+
+//start the test
 int main(int argc, char* argv[]) {
+
+  //test number
+  unit("number", []() -> bool {
+    Symbol symbol(symbolType::NUMBER);
+
+    *symbol.value = 42;
+    bool whole = !toString(symbol).compare("42");
+
+    *symbol.value = 42.3;
+    bool decimal = !toString(symbol).compare("42.3");
+
+    return whole && decimal;
+  });
+
+  //test string
+  unit("string", []() -> bool {
+    Symbol symbol(symbolType::STRING);
+    *symbol.value = "foobar";
+    return !toString(symbol).compare("foobar");
+  });
+
+  //test reference
+  unit("reference", []() -> bool {
+    Symbol symbol;
+    symbol.type = symbolType::REFERENCE;
+
+    Symbol example(symbolType::NUMBER);
+    *example.value = 42;
+
+    *symbol.value = &example;
+
+    return !toString(symbol).compare("&42");
+  });
+
   //test array
-  {
+  unit("array", []() -> bool {
     Symbol symbol;
     symbol.type = symbolType::ARRAY;
 
@@ -20,73 +65,45 @@ int main(int argc, char* argv[]) {
 
     *member1.value = 1;
     *member2.value = 2;
-    *member3.value = 3;
+    *member3.value = 3.33;
     *member4.value = 4;
     *member5.value = 5;
 
-    *symbol.value = {member1, member2, member3, member4, member5};
+    *symbol.value = {
+      member1,
+      member2,
+      member3,
+      member4,
+      member5
+    };
 
-    std::cout << "array:\t" << toString(symbol) << std::endl;
-  }
+    return !toString(symbol).compare("[1,2,3.33,4,5]");
+  });
 
-  //test function
-  {
-    Symbol symbol;
-
-    symbol.type = symbolType::FUNCTION;
-    //TODO: set a function
-
-    std::cout << "func:\t" << toString(symbol) << std::endl;
-  }
-
-  //test number
-  {
-    Symbol symbol(symbolType::NUMBER);
-    *symbol.value = 42;
-    std::cout << "number:\t" << toString(symbol) << std::endl;
-  }
-
-  //test string
-  {
-    Symbol symbol(symbolType::STRING);
-    *symbol.value = "foobar";
-    std::cout << "string:\t" << toString(symbol) << std::endl;
-  }
-
-  //test reference
-  {
-    Symbol symbol;
-    symbol.type = symbolType::REFERENCE;
-
-    Symbol example(symbolType::NUMBER);
-    *example.value = 42;
-
-    *symbol.value = &example;
-
-    std::cout << "ref:\t" << toString(symbol) << std::endl;
-  }
-
-  /*
-  //any circular reference passed to toString will segfault
-  Symbol s, r;
-  s.type = symbolType::ARRAY;
-  r.type = symbolType::REFERENCE;
-  *r.value = &s;
-  *s.value = {r};
-  std::cout << toString(s) << std::endl;
-  */
+  //TODO: test function
 
   //test keywords & operators
-  for (int i = 5; i < 50; i++) {
-    Symbol symbol;
-    symbol.type = symbolType(i);
-    std::cout << i << "\t" << toString(symbol) << std::endl;
+  for (int i = 5; i < symbolType::SYMBOL_LAST; i++) {
+    unit("", [i]() -> bool {
+      Symbol symbol;
+      symbol.type = symbolType(i);
+
+      std::cout << toString(symbol);
+
+      return !toString(symbol)
+        .compare(symbolConst.at(symbolType(i)));
+    });
   }
+
+  std::cout << "Total fails: " << failCount << std::endl;
+
   return 0;
 }
 
 std::string toString(Symbol symbol) {
+  //any circular reference passed to toString will segfault
   switch(symbol.type) {
+
     //var types
     case symbolType::ARRAY: {
       std::string ret;
@@ -112,43 +129,42 @@ std::string toString(Symbol symbol) {
       return ret;
     }
 
-    case symbolType::NUMBER:
-        return std::to_string(symbol.value->number);
+    case symbolType::NUMBER: {
+      //get the string
+      std::string str = std::to_string(
+        symbol.value->number
+      );
+
+      //erase trailing zeroes
+      str.erase(
+        str.find_last_not_of('0') +1,
+        std::string::npos
+      );
+
+      //handle dangling period
+      if (str.back() == '.') {
+        str.pop_back();
+      }
+
+      return str;
+    }
 
     case symbolType::REFERENCE:
-        return std::string() +
-          toString(symbolType::REFERENCE_MARK) +
-          toString(*(symbol.value->reference));
+      return std::string() +
+        toString(symbolType::REFERENCE_MARK) +
+        toString(*(symbol.value->reference));
 
     case symbolType::STRING:
       return symbol.value->string;
 
     //keywords & operators
     default:
-      if (symbol.type < symbolType::SYMBOL_LAST) {
-        return symbolConst.at(symbol.type);
-      }
-      else {
-        //unknown symbol
-        return "unknown";
-      }
+    if (symbol.type < symbolType::SYMBOL_LAST) {
+      return symbolConst.at(symbol.type);
+    }
+    else {
+      //unknown symbol
+      return "unknown";
+    }
   };
-}
-
-Symbol parseString(std::string) {
-  //TODO: incomplete
-}
-
-int tokenize(std::string str, std::list<Symbol>& target) {
-  while(str.size()) {
-    //TODO: incomplete
-  }
-
-  return 0;
-}
-
-char popFront(std::string& str) {
-  char ret = str[0];
-  str = str.substr(1, str.size()-1);
-  return ret;
 }
